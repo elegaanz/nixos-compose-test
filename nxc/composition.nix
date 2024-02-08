97507a95816844ef750fb8a0a4ed1fa59f235e33
Dorian.Mounier@univ-grenoble-alpes.fr <dorian.mounier@etu.univ-grenoble-alpes.fr>
2024-02-08 10:49:54 +0100

    Add vector works in composition.nix with some comments

--- nxc/composition.nix
+++ nxc/composition.nix
@@ -1,20 +1,49 @@
-{ pkgs, ... }: {
-  roles = {
-    foo = { pkgs, ... }:
-      {
-        environment.systemPackages = with pkgs; [ opensearch ];
-        services.opensearch.enable = true;
-        services.nginx.enable = true;
-        services.openssh.authorizedKeysCommandUser = [
-          ./cle.pub
+{ pkgs, ... }: { 
+
+  roles = { 
+
+    foo = { pkgs, config, lib, ... }:  
+
+      {  
+
+        environment.systemPackages = with pkgs; [ opensearch vector ]; 
+
+        services.opensearch.enable = true;  
+
+        services.vector = { 
+          enable = true;  
+          settings = { 
+            sources = {  
+              "in" = { 
+                type = "stdin";  
+              };
+            };
+            sinks = {  
+              out = {  
+                inputs = ["in"]; 
+                type = "console";  
+                encoding = { 
+                  codec = "text";  
+                };
+              };
+            };
+          };
+        };
+
+        services.opensearch.extraJavaOptions= [  # Configuration des options Java supplémentaires pour le service "opensearch"
+          "-Xmx512m"  # Limite maximale de la mémoire utilisée par la machine virtuelle Java à 512 Mo
+          "-Xms512m"  # Mémoire initiale allouée par la machine virtuelle Java à 512 Mo
         ];
-        services.opensearch.extraJavaOptions= [
-          "-Xmx512m"
-          "-Xms512m"
-        ];
-      };
+
+        environment.variables = {  # Définition des variables d'environnement
+          VECTOR_CONFIG = lib.lists.last (  # La variable "VECTOR_CONFIG" est définie comme le dernier élément de la liste obtenue en divisant la chaîne "config.systemd.services.vector.serviceConfig.ExecStart" par des espaces
+            builtins.split " " config.systemd.services.vector.serviceConfig.ExecStart
+          );
+        };
+      };  
   };
-  testScript = ''
+
+  testScript = ''  
     foo.succeed("true")
   '';
 }
