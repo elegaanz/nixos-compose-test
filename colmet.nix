27a7addf2ad94235f186791b3e739f80ba2fc61b
Ana Gelez <ana@gelez.xyz>
2024-03-07 11:38:51 +0100

    Use colmet from nur-kapack

--- colmet.nix
+++ colmet.nix
@@ -1,55 +1,13 @@
 { lib, pkgs, config,  ... }:
 
 let
-  colmet = pkgs.python3Packages.buildPythonApplication rec {
-    name = "colmet-${version}";
-    version = "0.6.10.dev0";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "oar-team";
-      repo = "colmet";
-      rev = "a1c1c32e9f6014ddf8253842df27579b46b9a2fa";
-      sha256 = "sha256-wXsJtvml6PdG1V5hE7wmpdEcAvof6U1NPc1rFR15NHU=";
-    };
-
-    buildInputs = [ pkgs.powercap ];
-
-    propagatedBuildInputs = with pkgs.python3Packages; [
-      pyinotify
-      pyzmq
-      tables
-      requests
-    ];
-
-    preBuild = ''
-      mkdir -p $out/lib
-      sed -i "s#/usr/lib/#$out/lib/#g" colmet/node/backends/perfhwstats.py
-      sed -i "s#/usr/lib/#$out/lib/#g" colmet/node/backends/RAPLstats.py
-      sed -i "s#/usr/lib/#$out/lib/#g" colmet/node/backends/lib_perf_hw/makefile
-      sed -i "s#/usr/lib/#$out/lib/#g" colmet/node/backends/lib_rapl/makefile
-    '';
-
-    # Tests do not pass
-    doCheck = false;
-
-    patches= [ ./colmet-hwm-constants.patch ];
-
-    meta = with lib; {
-      description = "Collecting metrics about process running in cpuset and in a distributed environnement";
-      homepage = https://github.com/oar-team/colmet;
-      platforms = pkgs.powercap.meta.platforms;
-      licence = licenses.gpl2;
-      longDescription = ''
-    '';
-    };
-  };
   cfg-collector = config.services.colmet-collector;
   cfg-node = config.services.colmet-node;
   auth-file = pkgs.writeText "colmet-opensearch-auth" "admin:admin";
 in
 with lib;
 {
-  config.environment.systemPackages = [ colmet ];
+  config.environment.systemPackages = with pkgs; [ nur.repos.kapack.colmet ];
 
   #option to enable the colmet collector
   options.services.colmet-collector = {
@@ -67,7 +25,7 @@ with lib;
     wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" (mkIf config.services.opensearch.enable "opensearch.service") ];
     serviceConfig = {
-      ExecStart = "${colmet}/bin/colmet-collector -vvv " +
+      ExecStart = "${pkgs.nur.repos.kapack.colmet}/bin/colmet-collector -vvv " +
         "--zeromq-bind-uri tcp://0.0.0.0:5556 " +
         "--buffer-size 5000 " +
         "--sample-period 3 " +
@@ -84,7 +42,7 @@ with lib;
     wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" (mkIf config.services.opensearch.enable "opensearch.service") ];
     serviceConfig = {
-      ExecStart = "${colmet}/bin/colmet-node -vvv --zeromq-uri tcp://127.0.0.1:5556";
+      ExecStart = "${pkgs.nur.repos.kapack.colmet}/bin/colmet-node -vvv --zeromq-uri tcp://127.0.0.1:5556";
     };
   };
 }
--- opensearch-mono/flake.nix
+++ opensearch-mono/flake.nix
@@ -4,15 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nxc.url = "github:elegaanz/nixos-compose/fix-vde-switch-groups";
+    kapack.url = "github:oar-team/nur-kapack";
+    nur.url = "github:nix-community/nur";
   };
 
-  outputs = { self, nixpkgs, nxc }:
+  outputs = { self, nixpkgs, nxc, kapack, nur }:
     let
       system = "x86_64-linux";
     in
     {
       packages.${system} = nxc.lib.compose {
         inherit nixpkgs system;
+        NUR = nur;
+        repoOverrides = { inherit kapack; };
         composition = ./composition.nix;
       };
 
